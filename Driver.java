1d056f1c6652350693f2b22087bc2b70c13d788e
sookiemonster <dsooknanan20@stuy.edu>
2020-11-02 19:25:22 -0500

    Added get() method and tests for it.

--- Driver.java
+++ Driver.java
@@ -2,9 +2,12 @@ public class Driver {
   public static void main(String[] args) {
     SuperArray arr = new SuperArray();
     System.out.println(arr.size());
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 10; i++) {
       arr.add(Integer.toString(i));
       System.out.println("Added: " + i + " |  Size: " + arr.size());
+    }
+    for (int i = 0; i < 10; i++) {
+      System.out.println(arr.get(i));
     }
   }
 }
